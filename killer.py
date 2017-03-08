d47f04c42bba2235cecf31f47be28516fa186190
tkmru <i.am.tkmru@gmail.com>
2017-03-09 07:53:10 +0900

    add recv and send

--- killer.py
+++ killer.py
@@ -5,10 +5,29 @@ import socket
 import struct
 
 
-def connect(ip_addr, port):
+def remote(ip_addr, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ip_addr, port))
     return s
+
+
+def disconnect(s):
+    s.shutdown(socket.SHUT_WR)
+
+
+def recvuntil(s, word):
+    res = ''
+    while not res.endswith(word):
+        res += s.recv(1)
+    return res
+
+
+def recvline(s):
+    return recvuntil(s, '\n')
+
+
+def sendline(s, req):
+    s.sendall(req + '\n')
 
 
 def p32(number):
